7a2540089f3a9a6f813aba993a15f52292468fae
denisenko_ilya <07.iden@gmail.com>
2020-03-24 02:54:44 +0300

    implemented validateAccountData() method

--- SaveAccountDataAction.js
+++ SaveAccountDataAction.js
@@ -1,28 +1,47 @@
 const {BaseAction} = require("./BaseAction");
+const _ = require('lodash');
 
 class SaveAccountDataAction extends BaseAction {
     /**
      *
-     * @param name
-     * @param cashFlow
-     * @param trades
-     * @param operations
+     * @param {string} name
+     * @param {Array} cashFlow
+     * @param {Array <{tradeNo: number, margin: number}>} trades
+     * @param {Array} operations
      */
     constructor({name, cashFlow, trades, operations}) {
         super();
         this.name = name;
-        this.cashFlow = cashFlow;
-        this.trades = trades;
-        this.operations = operations;
+        this.cashFlow = cashFlow ? cashFlow : [];
+        this.trades = trades ? trades : [];
+        this.operations = operations ? operations : [];
 
     }
-    run () {
+
+    run() {
         this.validateAccountData();
+        this.prepareAccountData();
     }
+    get reportName () {
+        return this.name;
+    }
+
     validateAccountData() {
-
+        const tradesMargin = +this.trades
+            .reduce((sum, item) => {
+                return sum + item.margin
+            }, 0).toFixed(2);
+        if (this.cashFlow.margin && this.cashFlow.margin !== tradesMargin) {
+            throw new Error(
+                `Cash flow margin "${this.cashFlow.margin}" does not match trades sum margin "${tradesMargin}"`);
+        }
+        console.log('  ' + this.trades.length, tradesMargin, this.cashFlow.margin)
     }
 
+    prepareAccountData() {
+        this.nonTrades = _.remove(this.trades, trade => !trade.tradeNo);
+
+    }
 }
 
 exports.SaveAccountDataAction = SaveAccountDataAction;
